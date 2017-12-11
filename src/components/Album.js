4c858cb9020b178064e5b1f2d0a66107493bc8e2
cheneyshreve <cheneyshreve@users.noreply.github.com>
2017-12-11 09:19:11 -0800

    checkpoint_07 completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,8 +10,38 @@ import albumData from './../data/albums';
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause(){
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song){
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong ){
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song);}
+      this.play();
+    }
   }
 
   render(){
@@ -34,7 +64,7 @@ import albumData from './../data/albums';
         <tbody>
         {
           this.state.album.songs.map ( (song, index) =>
-          <tr>
+          <tr className="song" key={index} onClick={()=> this.handleSongClick(song)}>
             <td>
             <button>
               <span className="song-number">{index + 1} </span>
